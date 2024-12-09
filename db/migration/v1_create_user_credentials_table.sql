0fbbfdf6221a4be4061a49809936622515f39a39
= <raksmeykoung@gmail.com>
2024-12-09 20:54:11 +0700

    compatable with backed db migration

--- db/migration/v1_create_user_credentials_table.sql
+++
@@ -1,9 +0,0 @@
-    -- V1__create_users_table.sql
-
-    CREATE TABLE loans (
-        id serial primary key,
-        loan_no varchar(10) not null unique,
-        borrower_id int not null ,
-        amount decimal(10, 2),
-        interest_rate decimal (5, 2)
-    );
